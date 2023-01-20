34300b3c6bff73d1470bed136bd6f480d3b6882d
emmaz12 <ezc7288@gmail.com>
2023-01-19 23:05:22 -0800

    update
    update

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
-public class SearchEngine implements URLHandler{
+class Handler implements URLHandler{
 
     ArrayList <String> words = new ArrayList<String>();
 
@@ -12,30 +12,34 @@ public class SearchEngine implements URLHandler{
             String [] arr = url.getQuery().split("=");
             words.add(arr[1]);
         }
+        
+        else if (url.getPath().equals("/print")) {
+            return words.toString();
+        }
 
         else if (url.getPath().equals("/search")) {
             String [] arr = url.getQuery().split("=");
             
             for (String word: words){
                 if (word.contains(arr[1])) {
-                    result = result + word;
+                    result = result + " " + word;
                 }
             }
         }
         return result;
     }
-
-    class NumberServer {
-        public static void main(String[] args) throws IOException {
-            if(args.length == 0){
-                System.out.println("Missing port number! Try any number between 1024 to 49151");
-                return;
-            }
-    
-            int port = Integer.parseInt(args[0]);
-    
-            Server.start(port, new Handler());
-        }
-    }
-    
 }
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
+    
